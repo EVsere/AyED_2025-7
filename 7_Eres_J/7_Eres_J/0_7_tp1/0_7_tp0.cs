bdc488120633e0c4c0f4838256d8528387029adb
EVsere <emavictoriaeres@gmail.com>
2025-04-24 14:29:09 -0300

    Subida de carpeta 7_Eres_J

---
+++ 7_Eres_J/7_Eres_J/0_7_tp1/0_7_tp0.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_7_tp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TP1 ET0
+            //Utilizando unavariable tipo string un programa que almacene en una variable una cadena que sea ""Hola mundo!!" e imprimirla
+
+            //Caso 1
+            string variable1;
+            variable1 = "Hola mundo!!";
+            Console.ReadLine(variable1);
+            Console.ReadKey();
+
+            //Caso 2
+
+            string variable2 = "Hola mundo!!";
+            Console.ReadLine(variable2);
+            Console.ReadKey();
+        }
+    }
+}
