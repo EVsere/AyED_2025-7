869f4879003d7fee14c6a9b8d8bfc32a8a4fb6bb
EVsere <emavictoriaeres@gmail.com>
2025-09-30 15:15:44 -0300

    ya subo todito

---
+++ 7_Eres_J/7_Eres_J/3_pc7_0/3_pc7_0/3_pc7_0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_pc7_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Como te llamas?");
+            string nombre = Console.ReadLine();
+            Console.WriteLine(decirtext(nombre));
+            Console.ReadKey();
+        }
+        static string decirtext(string nombre)
+        {
+
+            return "Hola " + nombre;
+        }
+    }
+}
+
---
+++ 7_Eres_J/7_Eres_J/3_pc7_1/3_pc7_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_pc7_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("decime un numero");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("decime otro numero");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(num1+"+"+num2+"="+suma(num1, num2));
+            Console.ReadKey();
+        }
+        static int suma(int num1,int num2)
+        {
+
+            return num1+num2;
+        }
+    }
+}
+
